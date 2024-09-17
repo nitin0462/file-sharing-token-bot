e1eb34060167261562e6d588cb0e53849ff5adcf
nitin0462 <nitinyadav046298@gmail.com>
2024-09-18 01:44:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,15 +7,13 @@ TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "7460381086:AAHqAyjcwID48jC_m6iUUX
 
 #Your API ID & API HASH from my.telegram.org [https://youtu.be/gZQJ-yTMkEo?si=H4NlUUgjsIc5btzH]
 #Your API ID from my.telegram.org
-APP_ID = int(os.environ.get("APP_ID", "28123695
-"))
+APP_ID = int(os.environ.get("APP_ID", "28123695"))
 
 #Your API Hash from my.telegram.org
-API_HASH = os.environ.get("API_HASH", "6fcc0acacee23599eaa91208c854e879
-")
+API_HASH = os.environ.get("API_HASH", "6fcc0acacee23599eaa91208c854e879")
 
 #Your db channel Id
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "2378421557"))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002249190886"))
 
 #OWNER ID
 OWNER_ID = int(os.environ.get("OWNER_ID", "6922646979"))
@@ -25,15 +23,15 @@ PORT = os.environ.get("PORT", "8585")
 
 #Database 
 #Database [https://t.me/+e_w3AfIh91wzZjhl]
-DB_URI = os.environ.get("DATABASE_URL", "https://t.me/+e_w3AfIh91wzZjhl")
-DB_NAME = os.environ.get("DATABASE_NAME", "Ex db")
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://folk:Cluster0@cluster0.dyiy7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DB_NAME = os.environ.get("DATABASE_NAME", "cluster0")
 
 #Shortner (token system) 
 # check my discription to help by using my refer link of shareus.io
 # 
 
 SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "publicearn.com")
-SHORTLINK_API = os.environ.get("SHORTLINK_API", "48962cd7932756e4ea3948f9fdacbd982d4fae40")
+SHORTLINK_API = os.environ.get("SHORTLINK_API", "971f149c5c6978f6638759fe769a54bd695cf704")
 VERIFY_EXPIRE = int(os.environ.get('VERIFY_EXPIRE', 86400)) # Add time in seconds
 IS_VERIFY = os.environ.get("IS_VERIFY", "True")
 TUT_VID = os.environ.get("TUT_VID", "https://t.me/+PZfHvzjSiZc4OGE1") # shareus ka tut_vid he 
@@ -46,11 +44,11 @@ TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 #start message
 START_MSG = os.environ.get("START_MESSAGE", "Hello {first}\n\nI can store private files in Specified Channel and other users can access it from special link.")
 try:
-    ADMINS=[6922646979]
-    for x in (os.environ.get("ADMINS", "1480923991 5069922547 6695586027").split()):
-        ADMINS.append(int(x))
+    ADMINS=[6922646979]
+    for x in (os.environ.get("ADMINS", "5197673717 5069922547 6695586027").split()):
+        ADMINS.append(int(x))
 except ValueError:
-        raise Exception("Your Admins list does not contain valid integers.")
+        raise Exception("Your Admins list does not contain valid integers.")
 
 #Force sub message 
 FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {first}\n\n<b>You need to join in my Channel/Group to use me\n\nKindly Please join Channel</b>")
@@ -68,25 +66,25 @@ BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
 USER_REPLY_TEXT = "❌Don't send me messages directly I'm only File Share bot!"
 
 ADMINS.append(OWNER_ID)
-ADMINS.append(6695586027)
+ADMINS.append(5197673717)
 
 LOG_FILE_NAME = "filesharingbot.txt"
 
 logging.basicConfig(
-    level=logging.INFO,
-    format="[%(asctime)s - %(levelname)s] - %(name)s - %(message)s",
-    datefmt='%d-%b-%y %H:%M:%S',
-    handlers=[
-        RotatingFileHandler(
-            LOG_FILE_NAME,
-            maxBytes=50000000,
-            backupCount=10
-        ),
-        logging.StreamHandler()
-    ]
+    level=logging.INFO,
+    format="[%(asctime)s - %(levelname)s] - %(name)s - %(message)s",
+    datefmt='%d-%b-%y %H:%M:%S',
+    handlers=[
+        RotatingFileHandler(
+            LOG_FILE_NAME,
+            maxBytes=50000000,
+            backupCount=10
+        ),
+        logging.StreamHandler()
+    ]
 )
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 
 def LOGGER(name: str) -> logging.Logger:
-    return logging.getLogger(name)
+    return logging.getLogger(name)
